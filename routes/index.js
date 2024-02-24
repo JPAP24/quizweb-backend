2d91d5db30fb514307720393b82642bcfbbf1c21
mervs17 <merjs17@gmail.com>
2024-02-25 00:23:42 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 const AdminController = require("../controllers/AdminController");
 const UserController = require("../controllers/UserController");
-const {verifyToken} = require("../middleware/adminMiddleware");
+const { verifyToken } = require("../middleware/adminMiddleware");
 
 const express = require("express");
 const router = express.Router();
 
-// admin
+// admin routes
 router.post("/admin/login", AdminController.loginAdmin);
 router.post("/admin/register", AdminController.registerAdmin);
 router.get("/admin/user", verifyToken, AdminController.getAdminDetails);
 router.post("/admin/create/quiz", verifyToken, AdminController.setUpQuiz);
+router.post(
+  "/admin/create/question",
+  verifyToken,
+  AdminController.setUpQuestion
+);
 router.get("/admin/quizzes", verifyToken, AdminController.getQuizzes);
+router.get("/admin/question", verifyToken, AdminController.getQuestion);
 
-// user
+// user routes
 router.post("/user/add", verifyToken, UserController.registerUser);
 router.get("/users/get", verifyToken, UserController.getUsers);
 
