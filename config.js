2bf60b2271b7f88e50b033e46bfde5e07503110b
James Paul A. Patawaran <152288098+JPAP24@users.noreply.github.com>
2024-04-10 14:03:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   db: {
     host: "localhost",
-    user: "root",
-    password: "",
-    database: "quiz-api",
+    user: "id22027563_jpap",
+    password: "@Sd123789",
+    database: "id22027563_quizapi",
   },
 };
